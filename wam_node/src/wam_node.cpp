97111053ed7776ec9b39f1ad0adbb33ef4691a2b
Kevin Lam <kevin.lam@edu.uwaterloo.ca>
2018-05-10 17:01:57 -0400

    Added hand close before shutdown to avoid collision with arm

--- wam_node/src/wam_node.cpp
+++ wam_node/src/wam_node.cpp
@@ -888,6 +888,14 @@ template<size_t DOF>
     }
   }
 
+template<size_t DOF>
+  bool WamNode<DOF>::forceHandCloseSpread()
+  {
+    ROS_INFO("Closing the BarrettHand Spread");
+    hand->close(Hand::SPREAD, false);
+    return true;
+  }
+
 //wam_main Function
 template<size_t DOF>
   int wam_main(int argc, char** argv, ProductManager& pm, systems::Wam<DOF>& wam)
@@ -908,6 +916,8 @@ template<size_t DOF>
       wam_node.updateRT(pm);
       pub_rate.sleep();
     }
+
+    wam_node.forceHandCloseSpread();
 
     return 0;
   }
